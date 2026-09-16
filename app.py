ef6bb2cbc0b603e9a867fa856e88c7f2e235ebb5
vybecodexx-sudo <vybecodexx@gmail.com>
2026-09-15 21:24:03 -0300

    Teste column

--- app.py
+++ app.py
@@ -17,23 +17,24 @@ df_copy = df.copy()
 
 #primeiro gráfico exibição
 st.subheader("1. Comparação de Headway por Sinalização")
-fig1 = gerar_grafico_1(df_copy)
-st.pyplot(fig1)
 
-st.markdown("Nesse gráfico podemos observar a diferença entre o headway(tempo de intervalo) de bloco fixo e o CBTC." \
-"Principais pontos" \
-"O gráfico que mostra o CBTC possui uma variação bem mais cocnentrada dos valores indicando que o serviço será mais pontual, previsível e com menos atrasos." \
-"A troca para o CBTC permite rodas mais trens por hora na mesma linha, sem custos com expansão de infraestrutura. ")
+col1, col2 = st.columns([2, 1])
+
+with col1:
+    fig1 = gerar_grafico_1(df_copy)
+    st.pyplot(fig1)
+
+with col2:
+    st.markdown("Neste primeiro gráfico, observa-se claramente a diferença de desempenho entre os dois sistemas no tempo de intervalo (headway) entre as composições. A visualização do CBTC destaca uma variação de valores muito mais concentrada, demonstrando que a operação sob esse sistema se torna significativamente mais pontual, previsível e protegida contra atrasos em cadeia. Sob a ótica operacional, a transição para a tecnologia CBTC possibilita rodar mais trens por hora na mesma linha existente, elevando a capacidade de transporte sem a necessidade de investimentos em expansão de infraestrutura física.")
 
 #segundo gráfico exibição
 st.subheader("2. Relação entre Velocidade e Ocupação")
-fig2 = gerar_grafico_2(df_copy)
-st.pyplot(fig2)
 
-st.markdown('Nesse gráfico é perceptivel a relação inversamente proporcional onde com maior velocidade o tempo de ocupação diminui já que quanto mais rápido o trem terminar o circuito menos tempo ele vai estar ocupando o mesmo.' \
-            
-'Principais pontos:' \
+col3, col4 = st.columns([2, 1])
 
-'Durante baixas velocidades o tempo de ocupação dispara, isso evidencia o impacto que as zonas de parada tem, manobra ou aproximação de estações para as trocas.' \
+with col3:
+    fig2 = gerar_grafico_2(df_copy)
+    st.pyplot(fig2)
 
-'Em velocidades mais altas o tempo de ocupação estabiliza nos níveis mínimos, isso garante qeu o trecho fique livre mais rapidamente para o trem seguinte prosseguir de forma segura.')
+with col4:
+    plt.markdown('O segundo gráfico evidencia uma clara relação inversamente proporcional: conforme a velocidade permitida aumenta, o tempo de ocupação do trecho diminui. Isso ocorre porque, quanto mais rápido o trem percorre o circuito, menos tempo ele permanece sobre ele. Em faixas de baixa velocidade, o tempo de ocupação dispara, ressaltando o grande impacto que zonas de parada, áreas de manobra ou trechos de aproximação de estações exercem sobre a circulação. Em contrapartida, quando a velocidade se eleva, o tempo de ocupação estabiliza em patamares mínimos, garantindo que a via seja liberada rapidamente para que o próximo trem prossiga com total segurança.')
